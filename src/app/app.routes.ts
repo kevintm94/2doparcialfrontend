f31dea7ed6e297cb1b523f60651424ce8766ac7e
Kevin Trujillo <kevin.tm1994@outlook.com>
2025-10-27 23:41:14 -0400

    feat(web): crear servicio empleados y consumir API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Empleados } from './empleados/empleados';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'empleados',
+        component: Empleados,
+        children: []
+    }
+];
